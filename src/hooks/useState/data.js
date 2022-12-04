eeb9ca044f7c7a70e7a766ddca3a7151fc4b2cd8
Harisali78 <aliharis2739@gmail.com>
2022-12-04 12:51:46 +0500

    everything about news api

---
+++ src/hooks/useState/data.js
@@ -0,0 +1,7 @@
+export const myPeople = [
+  { id: 1, name: "Aamir", age: 22 },
+  { id: 2, name: "Kashif", age: 21 },
+  { id: 3, name: "Uzair", age: 19 },
+  { id: 4, name: "Abdul Hannan", age: 19 },
+  { id: 5, name: "Ahmad", age: 20 },
+];
